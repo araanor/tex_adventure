b0d14d329212df787e492438d98700e5992bba15
aranor <abcdeflongu@gmail.com>
2025-02-04 23:03:38 +0000

    new start for terminal

---
+++ Code/Tex_Adventure.py
@@ -0,0 +1,14 @@
+import pickle
+import time
+import random
+
+
+def Tutorial():
+    Q = int(input("Do you want  to do the tutorial. 1 for yes, 2 for no"))
+    if Q == 1:
+        print("From here on out 1 is for yes, 2 is for no, attack can be used in battle, inv can be used to open your inventory anywhere.")
+    else:
+        pass
+
+
+
